2de58a1a4fa9df19b02f874c2bcf713a38a3492e
ABSoN <balazsking@hotmail.com>
2026-05-08 22:32:20 +0200

    Add REST API controller for Swagger documentation

---
+++ src/main/java/hu/kiralybalazs/realestate/controller/PropertyApiController.java
@@ -0,0 +1,31 @@
+package hu.kiralybalazs.realestate.controller;
+
+import hu.kiralybalazs.realestate.model.Property;
+import hu.kiralybalazs.realestate.service.PropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController // <-- Ez a kulcs a Swaggerhez!
+@RequestMapping("/api/properties") // <-- Külön útvonal az API-nak
+public class PropertyApiController {
+
+    @Autowired
+    private PropertyService propertyService;
+
+    @GetMapping
+    public List<Property> getAllProperties() {
+        return propertyService.findAll();
+    }
+
+    @PostMapping
+    public Property createProperty(@RequestBody Property property) {
+        return propertyService.save(property);
+    }
+
+    @GetMapping("/{id}")
+    public Property getPropertyById(@PathVariable Long id) {
+        return propertyService.findById(id).orElse(null);
+    }
+}
